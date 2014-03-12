c81a7c81091fc6af5a197ea6e41fadcee63a61a9
pawurb <p.urbanek89@gmail.com>
2014-03-12 20:04:53 +0100

    setup caching and avoid flickering bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Siorbackend::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # to avoid flickering images
+  config.serve_static_assets = true
 end
+
