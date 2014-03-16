9dbfaf940c81a423da8caa256ca9df30a2313683
pawurb <p.urbanek89@gmail.com>
2014-03-16 22:49:02 +0100

    movbed mailer config

--- config/application.rb
+++ config/application.rb
@@ -21,3 +21,21 @@ module Siorbackend
     # config.i18n.default_locale = :de
   end
 end
+
+Siorbackend::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "siorb.dobreziele.pl",
+    :enable_starttls_auto => true,
+    :authentication       => :plain,
+    :user_name            => ENV['SMTP_USERNAME'],
+    :password             => ENV['SMTP_PASSWORD'],
+    :openssl_verify_mode  => 'none'
+
+  }
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -4,18 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Siorbackend::Application.initialize!
 
-Siorbackend::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-    ActionMailer::Base.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => "siorb.dobreziele.pl",
-      :enable_starttls_auto => true,
-      :authentication       => :plain,
-      :user_name            => ENV['SMTP_USERNAME'],
-      :password             => ENV['SMTP_PASSWORD'],
-      :openssl_verify_mode  => 'none'
-
-    }
-end
