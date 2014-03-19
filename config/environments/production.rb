db39d2653a2b8703f36c5e5a943dccad4607b6d1
pawurb <p.urbanek89@gmail.com>
2014-03-19 17:38:51 +0100

    fix prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,8 @@ Siorbackend::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  ParagonParserV1::Application.config.middleware.use ExceptionNotification::Rack,
+  # Exception notification
+  config.middleware.use ExceptionNotification::Rack,
     :email => {
       :email_prefix => "[Siorbackend] ",
       :sender_address => %{"Siorbackend error" <pabloweb358@gmail.com>},
