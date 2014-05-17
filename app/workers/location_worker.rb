91603392f52d516f5747c468b0ec661124e71a57
pawurb <p.urbanek89@gmail.com>
2014-05-18 00:29:56 +0200

    quick fix

--- app/workers/location_worker.rb
+++ app/workers/location_worker.rb
@@ -5,7 +5,7 @@ class LocationWorker
   def perform(statistic_id)
     stat = Statistic.find(statistic_id)
     url = "http://ip-api.com/json/#{stat.ip}"
-    RestClient::Request.execute method: :get, url: url, timeout: 10, open_timeout: 10
+    response = RestClient::Request.execute method: :get, url: url, timeout: 10, open_timeout: 10
     stat.city = JSON.parse(response)['city']
     stat.save
   rescue => e
