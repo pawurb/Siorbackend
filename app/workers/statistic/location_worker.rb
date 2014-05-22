1177d49171a67c0c2bc6d377d351985304d40655
pawurb <p.urbanek89@gmail.com>
2014-05-22 15:18:50 +0200

    smarter now

--- app/workers/statistic/location_worker.rb
+++ app/workers/statistic/location_worker.rb
@@ -7,7 +7,7 @@ class Statistic::LocationWorker
     url = "http://ip-api.com/json/#{stat.ip}"
     response = RestClient::Request.execute method: :get, url: url, timeout: 10, open_timeout: 10
     location = JSON.parse(response, symbolize_names: true)[:city]
-    stat.city = location.blank? ? 'n/a' : location
+    stat.city = (location.nil? || location.blank?) ? 'n/a' : location
     stat.save
   rescue => e
     Rails.logger.error "Failed to get statistic location because of: #{e.message}"
