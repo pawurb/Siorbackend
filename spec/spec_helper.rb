fca78f6def3daf5f2451a0012ad5c1009205d4d4
pawurb <p.urbanek89@gmail.com>
2014-05-22 16:12:26 +0200

    nicer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -6,7 +5,7 @@ require 'rspec/autorun'
 require 'capybara/rspec'
 require 'helpers/authentication_helpers'
 require 'sidekiq/testing'
-Sidekiq::Testing.fake!
+Sidekiq::Testing.fake! #do not run sidekiq workers in specs
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
@@ -15,14 +14,10 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
-
   config.infer_base_class_for_anonymous_controllers = false
-
   config.order = "random"
 
   config.include Capybara::DSL
   config.include AuthenticationHelpers
-
 end
