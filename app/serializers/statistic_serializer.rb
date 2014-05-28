01b4f3d2ebc05962cae60534ef36171cbbb9aefe
pawurb <p.urbanek89@gmail.com>
2014-05-28 16:59:52 +0200

    kolejnosc

--- app/serializers/statistic_serializer.rb
+++ app/serializers/statistic_serializer.rb
@@ -1,5 +1,5 @@
 class StatisticSerializer < ActiveModel::Serializer
   include ::SerializerDateFormatter
 
-  attributes :id, :duration, :score, :ip, :created_at_txt, :created_at_unix, :city, :attempts, :updated_at_txt, :updated_at_unix
+  attributes :id, :duration, :score, :ip, :city, :attempts, :updated_at_txt, :updated_at_unix, :created_at_txt, :created_at_unix
 end
