74528a6cffb1fe42acdbf08f868bde4688a15e6a
pawurb <p.urbanek89@gmail.com>
2016-01-26 22:52:17 +0100

    quickfix

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -1,4 +1,6 @@
 class ContactMessagesController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def create
     @message = ContactMessage.new params.fetch(:contact_message)
     @sent = if @message.valid?
