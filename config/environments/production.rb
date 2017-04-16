180a017ba94b7d5d40c9d81e09ee8743a14451ce
pawurb <p.urbanek89@gmail.com>
2017-04-16 14:11:10 +0200

    no debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Siorbackend::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
