b83bf1759b62f4f7e08a87c66c2f0e74fafd81b2
pawurb <p.urbanek89@gmail.com>
2017-05-13 13:29:30 +0200

    comments order

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   validates :content, presence: true
   scope :approved, -> {
-    where(approved: true)
+    where(approved: true).order(created_at: :desc)
   }
 end
