7df37bb656839457b85d09c1e23769b0549d75e9
pawurb <p.urbanek89@gmail.com>
2017-07-17 14:38:44 +0200

    json format

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,6 +11,7 @@ module API
 
       respond_to do |format|
         format.js { render text: 'User statistics updated' }
+        format.json { render text: 'User statistics updated' }
         format.html { redirect_to root_path }
       end
     end
