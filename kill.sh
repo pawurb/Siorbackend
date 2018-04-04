17fa4fd5b1befb162f222a9f9b0d34ceb6655e7d
pawurb <p.urbanek89@gmail.com>
2018-04-04 18:27:27 +0200

    remove not needed

--- kill.sh
+++
@@ -1,1 +0,0 @@
-PID=$(cat /home/deploy/Siorbackend/tmp/pids/server.pid) && kill -9 $PID
--- restart.sh
+++
@@ -1,1 +0,0 @@
-RAILS_ENV=production rake assets:precompile && echo "precompiled" && ./kill.sh && echo "killed" ; echo "starting"  && ./start.sh
