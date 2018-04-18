628d7f9dea427e217c7cb733c740e59af3e2f901
pawurb <p.urbanek89@gmail.com>
2018-04-18 17:54:30 +0200

    fix redirect

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Siorbackend
     # load lib folder
     config.autoload_paths += %W(#{config.root}/lib/modules #{config.root}/lib)
     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-      r301 /.*/, Proc.new {|path, rack_env| "https://#{rack_env['SERVER_NAME']}#{path}" }, :if => Proc.new {|rack_env| (rack_env['SERVER_NAME'] =~ /www\./i)}
+      r301 /.*/, Proc.new {|path, rack_env| "https://siorb.dobreziele.pl#{path}" }, :if => Proc.new {|rack_env| (rack_env['SERVER_NAME'] =~ /www\./i)}
     end
   end
 end
